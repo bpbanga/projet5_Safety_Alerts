c6d2f2673d3cb25701843e59b32960a976bc421e
Pages Tchana <pagestchana@gmail.com>
2023-08-09 00:43:00 +0200

    première version

--- projet5/src/main/java/com/openclassroom/projet5/service/status/IChildAlertService.java
+++
@@ -1,8 +0,0 @@
-package com.openclassroom.projet5.service.status;
-import java.util.List;
-import com.openclassroom.projet5.model.status.ChildAlert;
-
-public interface IChildAlertService {
-	List<ChildAlert> getChildAlert(String address);
-
-}
--- projet5/src/main/java/com/openclassroom/projet5/service/status/IFireAddressAlertService.java
+++
@@ -1,9 +0,0 @@
-package com.openclassroom.projet5.service.status;
-
-import com.openclassroom.projet5.model.status.FireAddressAlert;
-
-public interface IFireAddressAlertService {
-
-	FireAddressAlert getFireAddressAlert(String address);
-
-}
